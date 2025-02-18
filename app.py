53dc9051d6011fba5b15cadc7adf8331e3ecff47
Sai Praneeth Bangarampally <saipraneethbangarampally@gmail.com>
2025-02-18 08:32:47 -0600

    first commit

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-
 db = SQLAlchemy(app)
 
-# Define the database model
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    complete = db.Column(db.Boolean, default=False)
 
-# Move the route function outside the class
 @app.route('/')
 def index():
     todo_list = Todo.query.all()
-    print(todo_list)  # For debugging
     return render_template('base.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:todo_id>')
+def update_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    todo = Todo.query.get(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-
-        # Insert a sample todo only if the table is empty
-        if not Todo.query.first():
-            new_todo = Todo(title="todo 1", complete=False)
-            db.session.add(new_todo)
-            db.session.commit()
-
     app.run(debug=True)
