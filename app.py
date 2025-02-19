35f8cca3af1f1eb4ba3d125196e680e1231c3c3e
Sai Praneeth Bangarampally <saipraneethbangarampally@gmail.com>
2025-02-19 15:52:34 -0600

    commit

--- app.py
+++ app.py
@@ -2,41 +2,48 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean, default=False)
+    complete = db.Column(db.Boolean)
 
-@app.route('/')
-def index():
+
+@app.route("/")
+def home():
     todo_list = Todo.query.all()
-    return render_template('base.html', todo_list=todo_list)
+    return render_template("base.html", todo_list=todo_list)
 
-@app.route('/add', methods=['POST'])
-def add_todo():
-    title = request.form.get('title')
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("home"))
 
-@app.route('/update/<int:todo_id>')
-def update_todo(todo_id):
-    todo = Todo.query.get(todo_id)
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("home"))
 
-@app.route('/delete/<int:todo_id>')
-def delete_todo(todo_id):
-    todo = Todo.query.get(todo_id)
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     with app.app_context():
